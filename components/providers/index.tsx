ebacc52e86e0cac3551536cc09f8db9bb9940edf
Leo Constantin <lcon69184@gmail.com>
2025-12-06 19:54:11 +0200

    added theme and fonts

---
+++ components/providers/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { AppProgressProvider } from "@bprogress/next";
+// import { Analytics } from "@vercel/analytics/next";
+// import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Toaster } from "@/components/ui/sonner";
+// import ConvexClientProvider from "../ConvexClientProvider";
+import { TailwindIndicator } from "../custom/tailwind-indicator";
+import { ThemeProvider } from "./theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    // <ConvexClientProvider>
+    // <NuqsAdapter>
+    <ThemeProvider>
+      <AppProgressProvider
+        color="var(--foreground)"
+        delay={500}
+        height="2px"
+        options={{ showSpinner: false }}
+      >
+        {children}
+      </AppProgressProvider>
+      <Toaster closeButton expand position="bottom-right" />
+      <TailwindIndicator />
+      {/* <Analytics /> */}
+    </ThemeProvider>
+    // </NuqsAdapter>
+    // </ConvexClientProvider>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: "FortuneX",
+  url: "https://fortunex-beta.vercel.app/", // Replace with production URL
+  ogImage: "https://fortunex-beta.vercel.app/opengraph-image.png",
+  description:
+    "Explore, compare, and connect with service providers in Rwanda effortlessly.",
+  links: {
+    twitter: "https://x.com/rathonrw",
+    linkedin: "https://www.linkedin.com/company/rathon",
+    instagram: "https://www.instagram.com/rathonrw/",
+    youtube: "https://www.youtube.com/@RathonRw",
+    email: "mailto:rathonrw@gmail.com",
+    whatsapp: "https://wa.me/250792636403",
+    github: "https://github.com/RathonRw",
+  },
+};
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#000000",
+};
